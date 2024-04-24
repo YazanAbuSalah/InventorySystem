3f91ef09659ac7960b43bb82633060b87a57e652
yazan abu salah <yazanhasanabusalah@gmail.com>
2024-04-24 15:08:27 +0300

    Add a product and inventory

---
+++ InventorySystem/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem
+{
+    public class Inventory
+    {
+        public List<Product> product { get; set; }
+
+        public Inventory()
+        {
+            product = new List<Product>();
+        }
+  
+
+        public void inventoryProduct() 
+        {
+            foreach (var item in product)
+            {
+                Console.WriteLine($"Product name : {item.Name}  ,  Product name : {item.Price}   ,   Product name : {item.Quantity} ");
+            }
+        }
+    }
+}
---
+++ InventorySystem/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace InventorySystem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter name of Product");
+            string name = Console.ReadLine();
+            Console.WriteLine("enter price of Product");
+            int  price = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter quantity of Product");
+            int quantity = int.Parse(Console.ReadLine());
+
+            Product p = new Product() { Name = name , Price = price , Quantity =quantity  };
+
+            Inventory inventory = new Inventory();
+            inventory.product.Add(p);
+            inventory.inventoryProduct();
+
+        }
+    }
+}
